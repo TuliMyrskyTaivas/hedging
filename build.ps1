044760b36ff4232ace1e36be6f8eaaeb3790be37
Andrey Sploshnov <ice@ice-castle.ru>
2024-12-21 22:49:21 +0300

    Enable CGO by default, restore the environment after the build

--- build.ps1
+++ build.ps1
@@ -2,4 +2,14 @@
 $now = Get-Date -UFormat "%Y-%m-%d_%T"
 $sha1 = (git rev-parse HEAD).Trim()
 
-go build -o hedging.exe -ldflags "-X main.sha1ver=$sha1 -X main.buildTime=$now"
+# Preserve the original path to restore it later
+$origPath = $env:Path
+
+# Run go build with specified environment
+$env:CGO_ENABLED=1
+$env:Path+=";C:\winlibs-mingw\mingw64\bin"
+go build -v -o hedging.exe -ldflags "-X main.sha1ver=$sha1 -X main.buildTime=$now"
+
+# Restore the environment
+$env:Path = $origPath
+Remove-Item Env:\CGO_ENABLED
