723807407e84188f61da0b08b809259a17f2268e
Andrey Sploshnov <ice@ice-castle.ru>
2025-04-19 12:49:15 +0300

    Move to Go 1.23.4, add first unit test

---
+++ hedging/locale_test.go
@@ -0,0 +1,28 @@
+package hedging
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestGetLocale(t *testing.T) {
+	locale, err := getLocale()
+	if err != nil {
+		t.Fatalf("getLocale failed: %s", err)
+	}
+
+	expected := regexp.MustCompile("[a-z]{2}-[A-Z]{2}")
+	if !expected.MatchString(locale) {
+		t.Fatalf("locale format mismatch: %s", locale)
+	}
+}
+
+func TestGetPrinter(t *testing.T) {
+	printer, err := GetPrinter()
+	if err != nil {
+		t.Fatalf("GetPrinter failed: %s", err)
+	}
+	if printer == nil {
+		t.Fatalf("GetPrinter unexpectedly returned nil")
+	}
+}
